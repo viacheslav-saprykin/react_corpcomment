eeaa26587e85eebea4b1e99d76209217f02a4e2b
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-05-30 18:03:24 +0300

    add: base validation

--- src/components/FeedForm.tsx
+++ src/components/FeedForm.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import { MAX_CHARACTERS } from '../lib/constans';
+
+
+
 export default function FeedForm() {
+  const [text, setText] = useState('');
+
+  const charCount = MAX_CHARACTERS - text.length;
+
   return (
     <form className="form">
       <textarea
+        value={text}
+        onChange={(event) => setText(event.target.value)}
         placeholder="sometext"
         id="feedback-textarea"
         spellCheck={false}
@@ -10,7 +21,7 @@ export default function FeedForm() {
         Enter your feedback here, remember to #hashtag the company.
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
