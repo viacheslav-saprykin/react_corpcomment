5cd7559d880f0a596a65af4cafd710dc06c8060c
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-06-09 16:01:02 +0300

    add: valid & invalid validators

--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -7,6 +7,8 @@ type FeedbackFormProps = {
 
 export default function FeedbackForm({ onAddToList }: FeedbackFormProps) {
   const [text, setText] = useState('');
+  const [showValidIndicator, setShowValidIndicator] = useState(false);
+  const [showInvalidIndicator, setShowInvalidIndicator] = useState(false);
   const charCount = MAX_CHARACTERS - text.length;
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,12 +21,27 @@ export default function FeedbackForm({ onAddToList }: FeedbackFormProps) {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    // basic validation
+    if (text.includes('#') && text.length >= 5) {
+      setShowValidIndicator(true);
+      setTimeout(() => setShowValidIndicator(false), 2000);
+    } else {
+      setShowInvalidIndicator(true);
+      setTimeout(() => setShowInvalidIndicator(false), 2000);
+      return;
+    }
     onAddToList(text);
     setText('');
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form">
+    <form
+      onSubmit={handleSubmit}
+      className={`form ${showValidIndicator ? ' form--valid' : ''}${
+        showInvalidIndicator ? ' form--invalid' : ''
+      }`}
+    >
       <textarea
         value={text}
         onChange={handleChange}
